50aa101358d2bb6bd8577df6aaace6ee8a3f5fb2
wlwlwon <ryanwon@naver.com>
2022-12-22 23:48:45 +0900

    [#1]feat:jwt Provider && jwtFilter 작성

---
+++ src/main/java/com/ecommerce/ecommerce/config/jwt/JwtProvider.java
@@ -0,0 +1,23 @@
+package com.ecommerce.ecommerce.config.jwt;
+
+import com.ecommerce.ecommerce.config.UserPrincipal;
+
+import com.ecommerce.ecommerce.domain.member.Member;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public interface JwtProvider {
+
+    String generateAccessToken(UserPrincipal auth);
+
+    String generateNewAccessToken(Member member);
+
+    String generateRefreshToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean isAccessTokenValid(HttpServletRequest request);
+
+}
