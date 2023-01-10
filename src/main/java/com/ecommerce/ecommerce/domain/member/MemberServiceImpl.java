1e41d650a182b7701d6117f21c9f11dd79327b01
wlwlwon <ryanwon@naver.com>
2023-01-10 11:51:06 +0900

    [#3]feat:Friend 친구추가 Add

--- src/main/java/com/ecommerce/ecommerce/domain/member/MemberServiceImpl.java
+++ src/main/java/com/ecommerce/ecommerce/domain/member/MemberServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ecommerce.ecommerce.domain.member;
 
+import com.ecommerce.ecommerce.domain.member.friend.Friend;
+import com.ecommerce.ecommerce.domain.member.friend.FriendRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,6 +31,23 @@ public class MemberServiceImpl implements MemberService{
         member.setAuthority(Role.Commerce_USER);
 
         return modelMapper.map(memberRepository.save(member), MemberResponseDTO.class);
+    }
+
+    @Override
+    public MemberResponseDTO addFriend(String currentMember, FriendRequestDTO friendRequestDTO) throws Exception{
+        Optional<Member> byEmail = memberRepository.findByEmail(currentMember);
+
+        if(byEmail.isEmpty()){
+            throw new Exception("멤버가 존재하지 않습니다.");
+        }
+        Member member = byEmail.get();
+
+        Friend friend = modelMapper.map(friendRequestDTO, Friend.class);
+
+        member.getFriendSet().add(friend);
+        memberRepository.save(member);
+        MemberResponseDTO memberResponseDTO = modelMapper.map(member, MemberResponseDTO.class);
+        return memberResponseDTO;
     }
 
 }
