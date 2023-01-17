4c6a8048107920008f19270f946274bdbedcb342
wlwlwon <ryanwon@naver.com>
2023-01-18 02:41:51 +0900

    [#5]refactor:Friend domain move

---
+++ src/main/java/com/ecommerce/ecommerce/domain/friend/FriendController.java
@@ -0,0 +1,28 @@
+package com.ecommerce.ecommerce.domain.friend;
+
+import com.ecommerce.ecommerce.config.UserPrincipal;
+import com.ecommerce.ecommerce.domain.member.MemberResponseDTO;
+import com.ecommerce.ecommerce.domain.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/friend")
+@RequiredArgsConstructor
+public class FriendController {
+
+    private final FriendService friendService;
+
+    @PostMapping("/addFriend")
+    public ResponseEntity<MemberResponseDTO> addFriend(@AuthenticationPrincipal UserPrincipal member, @RequestBody FriendRequestDTO friendRequestDTO) throws Exception{
+        MemberResponseDTO memberResponseDTO = friendService.addFriend(member.getUsername(),friendRequestDTO);
+        return new ResponseEntity<>(memberResponseDTO, HttpStatus.OK);
+    }
+
+}
