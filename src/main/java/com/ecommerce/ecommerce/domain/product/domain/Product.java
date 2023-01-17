584499eb459b2feb51cfc311e2cb1244f360caac
wlwlwon <ryanwon@naver.com>
2023-01-18 02:32:51 +0900

    [#4]feat:Product domain 추가

---
+++ src/main/java/com/ecommerce/ecommerce/domain/product/domain/Product.java
@@ -0,0 +1,50 @@
+package com.ecommerce.ecommerce.domain.product.domain;
+
+import com.ecommerce.ecommerce.domain.cart.domain.Cart;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private long categoryId;
+
+    @Column
+    private String name;
+
+    @Column
+    private long price;
+
+    @Column
+    private String mainImg;
+
+    @Column
+    private String detailImg;
+
+    @Column
+    private long stock;
+
+    @Column
+    private Float score;
+
+    @Column
+    private long deliveryFee;
+
+    @Column
+    private Boolean rocket;
+
+    @Column
+    private Boolean rocketFresh;
+
+    @Column
+    private Boolean rocketGlobal;
+
+}
