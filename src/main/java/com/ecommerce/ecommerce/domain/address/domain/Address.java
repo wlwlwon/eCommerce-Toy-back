8f143147974b03cd87206b8c6e06dfed712a256f
wlwlwon <ryanwon@naver.com>
2023-01-19 07:32:18 +0900

    [#11]feat: Address Domain 추가

---
+++ src/main/java/com/ecommerce/ecommerce/domain/address/domain/Address.java
@@ -0,0 +1,26 @@
+package com.ecommerce.ecommerce.domain.address.domain;
+
+import com.ecommerce.ecommerce.domain.member.domain.Member;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private boolean isMain;
+
+    private String name;
+
+    private String content;
+
+    @OneToOne
+    private Member member;
+
+}
