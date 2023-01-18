ed9ba88674f26573a991034f8a712fc98965c424
wlwlwon <ryanwon@naver.com>
2023-01-19 01:14:15 +0900

    [#9]refactor: Stuff path 수정

--- src/main/java/com/ecommerce/ecommerce/domain/stuff/repository/StuffRepository.java
+++ src/main/java/com/ecommerce/ecommerce/domain/stuff/repository/StuffRepository.java
@@ -1,7 +1,8 @@
-package com.ecommerce.ecommerce.domain.stuff;
+package com.ecommerce.ecommerce.domain.stuff.repository;
 
 import com.ecommerce.ecommerce.domain.cart.domain.Cart;
 import com.ecommerce.ecommerce.domain.product.domain.Product;
+import com.ecommerce.ecommerce.domain.stuff.domain.Stuff;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
