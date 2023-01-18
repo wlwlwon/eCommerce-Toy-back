8bc276eeb1b15da1f2d81ed09760e503f4b81ebb
wlwlwon <ryanwon@naver.com>
2023-01-19 01:08:01 +0900

    [#8]feat: Order dto 추가

---
+++ src/main/java/com/ecommerce/ecommerce/domain/order/domain/OrderProduct.java
@@ -0,0 +1,29 @@
+package com.ecommerce.ecommerce.domain.order.domain;
+
+import com.ecommerce.ecommerce.domain.stuff.domain.Stuff;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderProduct {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    private Order order;
+
+    @OneToOne
+    private Stuff stuff;
+
+    private long productId;
+
+    private int productNum;
+}
