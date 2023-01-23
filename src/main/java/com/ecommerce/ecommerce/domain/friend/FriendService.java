8a4e38d71366ce7fcc777238c862bc24da9e83ab
wlwlwon <ryanwon@naver.com>
2023-01-24 00:51:09 +0900

    [#14]feat: Friend domain 삭제

--- src/main/java/com/ecommerce/ecommerce/domain/friend/FriendService.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.ecommerce.domain.friend;
-
-import com.ecommerce.ecommerce.domain.member.dto.MemberResponseDTO;
-
-public interface FriendService {
-
-    MemberResponseDTO addFriend(String currentMember, FriendRequestDTO friendRequestDTO) throws Exception;
-}
