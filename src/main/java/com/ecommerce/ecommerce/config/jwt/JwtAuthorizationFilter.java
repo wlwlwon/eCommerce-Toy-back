cc67c10ca412b61b6d36b043e216026249ae8c1b
wlwlwon <ryanwon@naver.com>
2023-01-25 00:31:22 +0900

    [#15]feat: JwtFilter logout 구현

--- src/main/java/com/ecommerce/ecommerce/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/ecommerce/ecommerce/config/jwt/JwtAuthorizationFilter.java
@@ -1,9 +1,14 @@
 package com.ecommerce.ecommerce.config.jwt;
 
 
+import com.ecommerce.ecommerce.config.exception.CustomException;
+import com.ecommerce.ecommerce.config.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -18,12 +23,20 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtProvider jwtProvider;
 
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
         Authentication authentication = jwtProvider.getAuthentication(request);
         if (authentication != null && jwtProvider.isAccessTokenValid(request)) {
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String isLogout = redisTemplate.opsForValue().get(SecurityUtils.extractAuthTokenFromRequest(request));
+            if(ObjectUtils.isEmpty(isLogout))
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            else
+                throw new CustomException("사용할 수 없는 token입니다.", HttpStatus.BAD_REQUEST);
         }
 
         filterChain.doFilter(request, response);
