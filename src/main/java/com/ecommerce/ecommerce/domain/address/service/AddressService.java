8bda79762f708b4126810f18a719e31466a632d9
wlwlwon <ryanwon@naver.com>
2023-01-30 13:43:56 +0900

    [#16]refactor: AddressService addAddress 로직 수정

--- src/main/java/com/ecommerce/ecommerce/domain/address/service/AddressService.java
+++ src/main/java/com/ecommerce/ecommerce/domain/address/service/AddressService.java
@@ -2,8 +2,11 @@ package com.ecommerce.ecommerce.domain.address.service;
 
 import com.ecommerce.ecommerce.domain.address.domain.Address;
 import com.ecommerce.ecommerce.domain.address.dto.AddressRequestDTO;
+import com.ecommerce.ecommerce.domain.address.dto.AddressResponseDTO;
 import com.ecommerce.ecommerce.domain.member.domain.Member;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface AddressService {
-    Address addAddress(Member member, AddressRequestDTO addressRequestDTO);
+    @Transactional
+    AddressResponseDTO addAddress(Member member, AddressRequestDTO addressRequestDTO);
 }
