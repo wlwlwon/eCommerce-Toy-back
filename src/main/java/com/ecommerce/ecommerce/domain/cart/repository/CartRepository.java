ad4a2875ce5f74a63a4fef8a670b0cc379eb7b16
wlwlwon <ryanwon@naver.com>
2023-01-30 14:24:51 +0900

    [#16]refactor: CouponController 수정

--- src/main/java/com/ecommerce/ecommerce/domain/cart/repository/CartRepository.java
+++ src/main/java/com/ecommerce/ecommerce/domain/cart/repository/CartRepository.java
@@ -6,12 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface CartRepository extends JpaRepository<Cart,Long> {
+public interface CartRepository extends JpaRepository<Cart, Long> {
 
     Optional<Cart> findByMember(Member member);
 }
--- src/main/java/com/ecommerce/ecommerce/domain/coupon/controller/CouponController.java
+++ src/main/java/com/ecommerce/ecommerce/domain/coupon/controller/CouponController.java
@@ -2,10 +2,13 @@ package com.ecommerce.ecommerce.domain.coupon.controller;
 
 import com.ecommerce.ecommerce.config.UserPrincipal;
 import com.ecommerce.ecommerce.domain.coupon.domain.Coupon;
+import com.ecommerce.ecommerce.domain.coupon.domain.UserCoupon;
 import com.ecommerce.ecommerce.domain.coupon.dto.CouponRequestDTO;
+import com.ecommerce.ecommerce.domain.coupon.dto.UserCouponResponseDTO;
 import com.ecommerce.ecommerce.domain.coupon.service.CouponService;
 import com.ecommerce.ecommerce.domain.global.common.StatusEnum;
 import com.ecommerce.ecommerce.domain.global.common.SuccessResponse;
+import com.ecommerce.ecommerce.domain.member.domain.Member;
 import com.ecommerce.ecommerce.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,17 +25,19 @@ public class CouponController {
     private final MemberService memberService;
 
     @PostMapping("/create-coupon")
-    public SuccessResponse createCoupon(@AuthenticationPrincipal UserPrincipal member, @RequestBody CouponRequestDTO couponRequestDTO){
+    public SuccessResponse createCoupon(@AuthenticationPrincipal UserPrincipal member, @RequestBody CouponRequestDTO couponRequestDTO) {
         Coupon coupon = couponService.createCoupon(couponRequestDTO);
         return SuccessResponse.builder()
                 .status(StatusEnum.OK)
-                .message("사용가능한 쿠폰 목록 가져오기 성공")
+                .message("쿠폰 생성 성공")
                 .data(coupon)
                 .build();
     }
+
     @GetMapping("/available-coupons")
-    public SuccessResponse getAvailableCoupons(){
-        List<Coupon> coupons = couponService.getAvailableCoupons();
+    public SuccessResponse getAvailableCoupons(@AuthenticationPrincipal UserPrincipal member) {
+        Member loginMember = memberService.getMember(member.getUsername());
+        List<UserCouponResponseDTO> coupons = couponService.getAvailableCoupons(loginMember);
         return SuccessResponse.builder()
                 .status(StatusEnum.OK)
                 .message("사용가능한 쿠폰 목록 가져오기 성공")
@@ -40,9 +45,8 @@ public class CouponController {
                 .build();
     }
 
-
     @PostMapping("/available-coupons/{id}")
-    public SuccessResponse saveCoupon(@PathVariable("id") final Long id, @AuthenticationPrincipal UserPrincipal member){
+    public SuccessResponse saveCoupon(@PathVariable("id") final Long id, @AuthenticationPrincipal UserPrincipal member) {
         couponService.saveCoupon(id, memberService.getMember(member.getUsername()));
         return SuccessResponse.builder()
                 .status(StatusEnum.CREATED)
